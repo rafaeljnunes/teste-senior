dc5f1e19ab3cc9d6f499d6581ae8c818b87c1bcd
Rafael J. Nunes <rafaeljnunes@me.com>
2016-07-18 10:31:59 -0300

    Função Verifica se vai dar praia

--- dist/js/previsaoTempo.js
+++ dist/js/previsaoTempo.js
@@ -10,6 +10,7 @@ function salvaFavorito() {
 	var cidadeFav = $('#cidade').val();
 	localStorage.setItem("cidadeFavorita", cidadeFav); // save the item
 }
+
 //Retorna o Favorito no Banco Local
 function retornaFavorito(){
 	var texto =localStorage.getItem("cidadeFavorita"); // retrieve
@@ -17,67 +18,76 @@ function retornaFavorito(){
        console.log(texto)
 }
 
-//Retorna os dados ao enviar o formulário
-function recebePrevisao() {
-
-	var api = 'http://api.openweathermap.org/data/2.5/forecast/daily';
-	var params = {'q': $("#cidade").val(), 'APPID':  'a9f044768582be9f0ae65aae31aedb63', 'units': 'metric', 'cnt' : '5'};
-
-	$.getJSON( api + "?" + $.param(params), function( data ) {
-
-
-       //proximo 1
-       //var newData = new Date(data.list[0].dt);
-       //var dia = newData.getDay();
-       //var mes = newData.getMonth();
-       //var ano = newData.getFullYear();
-       //newData.setFullYear(ano, mes, dia);
-       // console.log(dia + '/' + mes + '/' + ano);
-       //console.log(newData);
-
-       // var dataFormatada = data.list[0].dt;
-       // var timestamp = moment.unix(dataFormatada);
-       // console.log( timestamp.format("DD/MM/YY") );
-
-       $("#data1").text(formatDate(data.list[0].dt));
-       $("#data1Maxima").text(data.list[0].temp.max+'º');
-       $("#data1Minima").text(data.list[0].temp.min+'º');
-
-       //proximo 2
-       $("#data2").text(formatDate(data.list[1].dt));
-       $("#data2Maxima").text(data.list[1].temp.max+'º');
-       $("#data2Minima").text(data.list[1].temp.min+'º');
-
-       //proximo 3
-       $("#data3").text(formatDate(data.list[2].dt));
-       $("#data3Maxima").text(data.list[2].temp.max+'º');
-       $("#data3Minima").text(data.list[2].temp.min+'º');
-
-       //proximo 4
-       $("#data4").text(formatDate(data.list[3].dt));
-       $("#data4Maxima").text(data.list[3].temp.max+'º');
-       $("#data4Minima").text(data.list[3].temp.min+'º');
-
-       //proximo 5
-       $("#data5").text(formatDate(data.list[4].dt));
-       $("#data5Maxima").text(data.list[4].temp.max+'º');
-       $("#data5Minima").text(data.list[4].temp.min+'º');
-
-       // //proximo 5
-       // $("#data6").text(data.list[5].dt_txt);
-       // $("#data6Maxima").text(data.list[5].main.temp_max+'º');
-       // $("#data6Minima").text(data.list[5].main.temp_min+'º');
-
-
-
-        
-
-        });
-
-}
-
+//Formata Data Timestamp
 function formatDate(aux){
        var dataFormatada = aux;
        var timestamp = moment.unix(dataFormatada);
        return timestamp.format("DD/MM/YY");
 }
+
+function vaiDarPraia(aux) {
+
+       var retorno;
+
+       for(x=0; x<7; x++){
+              var hoje=new Date(aux.list[x].dt);
+              var diaSemana= hoje.getDay();
+
+              if(diaSemana == 6 || diaSemana == 0){
+
+                     retorno = (parseFloat(aux.list[0].temp.max) >= 25) ? "Vai dar praia" : " Não vai dar praia";
+              }
+       }
+
+       return retorno;
+
+       var semana=new Array(6);
+       semana[0]='Domingo';
+       semana[1]='Segunda-Feira';
+       semana[2]='Terça-Feira';
+       semana[3]='Quarta-Feia';
+       semana[4]='Quinta-Feira';
+       semana[5]='Sexta-Feira';
+       semana[6]='Sábado';
+}
+
+//Retorna os dados ao enviar o formulário
+function recebePrevisao() {
+
+	var api = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+	var params = {'q': $("#cidade").val(), 'APPID':  'a9f044768582be9f0ae65aae31aedb63', 'units': 'metric', 'cnt' : '7'};
+
+	$.getJSON( api + "?" + $.param(params), function( data ) {
+
+       //Data Atual
+       $("#data1").text(formatDate(data.list[0].dt));
+       $("#data1Maxima").text(data.list[0].temp.max+'º');
+       $("#data1Minima").text(data.list[0].temp.min+'º');
+
+       //Proximo 2
+       $("#data2").text(formatDate(data.list[1].dt));
+       $("#data2Maxima").text(data.list[1].temp.max+'º');
+       $("#data2Minima").text(data.list[1].temp.min+'º');
+
+       //Proximo 3
+       $("#data3").text(formatDate(data.list[2].dt));
+       $("#data3Maxima").text(data.list[2].temp.max+'º');
+       $("#data3Minima").text(data.list[2].temp.min+'º');
+
+       //Proximo 4
+       $("#data4").text(formatDate(data.list[3].dt));
+       $("#data4Maxima").text(data.list[3].temp.max+'º');
+       $("#data4Minima").text(data.list[3].temp.min+'º');
+
+       //Proximo 5
+       $("#data5").text(formatDate(data.list[4].dt));
+       $("#data5Maxima").text(data.list[4].temp.max+'º');
+       $("#data5Minima").text(data.list[4].temp.min+'º');
+
+
+       $("#praia").text(vaiDarPraia(data));
+
+       });
+
+}
+
